3687ed60cb12418aec19944e97a65bfd874740df
aya-elhamdouny <52864131+aya-elhamdouny@users.noreply.github.com>
2021-10-24 15:18:11 +0200

    -populate Livedata to navigation drawble

---
+++ app/src/main/java/com/example/placebook/adapter/BookmarkAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.placebook.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.placebook.R
+import com.example.placebook.databinding.BookmarkItemBinding
+import com.example.placebook.ui.MapsActivity
+import com.example.placebook.viewmodel.MapsViewModel
+
+class BookmarkAdapter(
+    private var bookmarkList: List<MapsViewModel.BookemarkerView>?,
+    private val activity: MapsActivity
+) : RecyclerView.Adapter<BookmarkAdapter.Viewholder>() {
+
+    class Viewholder(val binding : BookmarkItemBinding, val activity: MapsActivity) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+    fun setBookmarkData(bookmark : List<MapsViewModel.BookemarkerView>){
+        this.bookmarkList = bookmark
+        notifyDataSetChanged()
+}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookmarkAdapter.Viewholder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = BookmarkItemBinding.inflate(layoutInflater , parent , false)
+        return Viewholder(binding , activity)
+    }
+
+    override fun onBindViewHolder(holder: BookmarkAdapter.Viewholder, position: Int) {
+      bookmarkList?.let { list ->
+      val bookmarkViewData = list[position]
+          holder.binding.root.tag = bookmarkViewData
+          holder.binding.bookmarkData = bookmarkViewData
+          holder.binding.bookmarkIcon.setImageResource(R.drawable.ic_baseline_info_24)
+
+
+
+      }
+    }
+
+    override fun getItemCount() = bookmarkList?.size  ?:0
+}
